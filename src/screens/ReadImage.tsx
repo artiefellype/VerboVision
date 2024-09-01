25dac6b56d348f9180438b70479f36c6c262d117
Valdir Junior <valdirbsj90@gmail.com>
2024-09-01 16:15:47 -0300

    feat. OCR funcionando

--- src/screens/ReadImage.tsx
+++ src/screens/ReadImage.tsx
@@ -1,9 +1,75 @@
-import React from "react";
-import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  TouchableOpacity,
+  ActivityIndicator,
+} from "react-native";
 import Navbar from "../components/Nav";
+import Constants from "expo-constants";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function ReadImage({ route, navigation }: any) {
-  const { imgUri } = route.params;
+  const { imgUri, img } = route.params;
+  const [textRecognized, setTextRecognized] = useState("");
+  const [loading, setLoading] = useState(true);
+  const googleApiKey =
+    Constants.manifest?.extra?.googleApiKey ||
+    Constants.expoConfig?.extra?.googleApiKey;
+
+  const textRecognition = async (img: string) => {
+    const body = JSON.stringify({
+      requests: [
+        {
+          image: {
+            content: img, // Passando a imagem como Base64
+          },
+          features: [
+            {
+              type: "TEXT_DETECTION",
+            },
+          ],
+        },
+      ],
+    });
+
+    const response = await fetch(
+      `https://vision.googleapis.com/v1/images:annotate?key=${googleApiKey}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: body,
+      }
+    );
+
+    if (!response.ok) {
+      setLoading(false);
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    const detections = result.responses[0].textAnnotations;
+
+    if (detections?.length > 0) {
+      return detections[0].description;
+    } else {
+      console.log("Nenhum texto detectado na imagem");
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    textRecognition(imgUri).then((data) => {
+      setTextRecognized(data || "Nenhum texto encontrado");
+      setLoading(false);
+      console.log("TEXTINHO: ", data);
+    });
+  }, [textRecognized]);
 
   return (
     <View style={styles.container}>
@@ -13,14 +79,24 @@ export default function ReadImage({ route, navigation }: any) {
         onMenuPress={() => navigation.navigate("History")}
       />
       <View style={styles.contentContainer}>
-        <Image source={{ uri: imgUri }} style={styles.photo} />
-        <View style={styles.textContainer}>
-          <Text style={styles.text}>Se o picapau tivesse comunicado a policia a gente nao estaria fazendo esse trabalho</Text>
-        </View>
+        <Image source={{ uri: img }} style={styles.photo} />
+        {loading ? (
+          <View style={styles.loadingContainer}>
+            <ActivityIndicator size="large" color="#6200EE" />
+            <Text style={styles.loadingText}>Processando imagem...</Text>
+          </View>
+        ) : (
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>{textRecognized}</Text>
+          </View>
+        )}
       </View>
 
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
-        <Text style={styles.text}>HOME</Text>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate("Home")}
+      >
+        <Text style={styles.text}><Icon name="arrow-left" size={30} color="#fafafa" /></Text>
       </TouchableOpacity>
     </View>
   );
@@ -34,13 +110,14 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     flex: 1,
+    width: '100%',
     flexDirection: "column",
     alignItems: "flex-start",
     padding: 20,
   },
   photo: {
-    width: 100,
-    height: 100,
+    width: 250,
+    height: 300,
     resizeMode: "contain",
     marginBottom: 20,
   },
@@ -48,13 +125,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-start",
     width: "100%",
+    padding: 20,
   },
   text: {
     fontSize: 18,
     color: "#FFFFFF",
+    fontWeight: "bold",
+    textAlign: "left"
   },
   button: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 30,
     right: 30,
     paddingVertical: 10,
@@ -65,5 +145,17 @@ const styles = StyleSheet.create({
     backgroundColor: "#6200EE",
     justifyContent: "center",
     alignItems: "center",
-  }
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+  },
+  loadingText: {
+    fontSize: 18,
+    color: "#FFFFFF",
+    marginTop: 10,
+  },
 });
